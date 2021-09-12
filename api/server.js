85eaa70faa59a3fc6aa7a1dd9927a615d490f445
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-11 18:58:42 -0700

    Routers connected to server

--- api/server.js
+++ api/server.js
@@ -3,10 +3,18 @@ const server = express();
 
 server.use(express.json());
 
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+server.use("*", (req, res, next) => {
+  next({ status: 400, message: "Page not found!" });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.stats || 500).json({ message: `Oh no!! ${err.message}` });
+});
 
 module.exports = server;
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 const server = require("./api/server");
 const port = process.env.PORT || 5000;
 
-console.log(process.env.PORT)
-
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
